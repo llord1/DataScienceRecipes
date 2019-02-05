96deda6fdb6c37d81fee1ce344752589dabdda85
Mark Newman <mark@trinetteandmark.com>
2019-02-05 08:16:48 -0500

    adjusted ttr/mattr does not make sense in this context

--- code/measure_ttr.py
+++ code/measure_ttr.py
@@ -1,6 +1,5 @@
 import csv
 import progressbar
-import re
 import tarfile
 from utils import list_tar_files, read_lines_from_tar_file
 from collections import namedtuple
@@ -9,9 +8,7 @@ from pathlib import Path
 from statistics import mean 
 
 # declare all the named tuples up front
-TTR = namedtuple('TTR', 'TTR MATTR500 MATTR1k ATTR AMATTR500 AMATTR1k')
-# https://stackoverflow.com/questions/4800665
-pattern = re.compile(u'^[^\W\d_]+$', re.UNICODE)
+TTR = namedtuple('TTR', 'TTR MATTR500 MATTR1k')
 
 def measure_ttr(corpus):
     """
@@ -19,11 +16,10 @@ def measure_ttr(corpus):
 
     * Type/Token Ratio
     * Moving Average Type/Token Ratio in 500 and 1k versions
-    * Adjusted versions of the above to include neither punctuation nor numeric words
 
     This measure assumes the below:
     
-    * Words are seperated by a space
+    * Tokens are seperated by a space
 
     Parameters
     ----------
@@ -46,12 +42,12 @@ def measure_ttr(corpus):
         with tarfile.open(corpus, 'r') as corpus:
             with open(measure, 'w', encoding = 'utf-8', newline = '') as measure:
                 measure = csv.writer(measure, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_ALL)    
-                measure.writerow(['id', 'TTR','MATTR500','MATTR1k','ATTR','AMATTR500','AMATTR1k'])
+                measure.writerow(['id', 'TTR','MATTR500','MATTR1k'])
                 for (tar_info, tar_file) in list_tar_files(corpus):
                     id = Path(tar_info.name).stem
                     lines = read_lines_from_tar_file(tar_file)
                     m = calculate_measure(lines)
-                    measure.writerow([id, m.TTR, m.MATTR500, m.MATTR1k, m.ATTR, m.AMATTR500, m.AMATTR1k])
+                    measure.writerow([id, m.TTR, m.MATTR500, m.MATTR1k])
                     bar.update(tar_info.offset_data + tar_info.size)
                 pass
             pass
@@ -65,17 +61,12 @@ def calculate_measure(lines):
 
     # https://stackoverflow.com/questions/952914
     flat_list = [item for sublist in lines for item in sublist]
-    adj_flat_list = filter_out_nonwords(flat_list)
 
     ttr = calculate_mattr(flat_list, len(flat_list))
     mattr500 = calculate_mattr(flat_list, 500)
     mattr1k = calculate_mattr(flat_list, 1000)
-
-    attr = calculate_mattr(adj_flat_list, len(adj_flat_list))
-    amattr500 = calculate_mattr(adj_flat_list, 500)
-    amattr1k = calculate_mattr(adj_flat_list, 1000)
-    
-    return TTR(round(ttr, 3), round(mattr500, 3), round(mattr1k, 3), round(attr, 3), round(amattr500, 3), round(amattr1k, 3))
+   
+    return TTR(round(ttr, 3), round(mattr500, 3), round(mattr1k, 3))
 
 def calculate_mattr(word_list, window_length):
 
@@ -120,12 +111,3 @@ def minus_one(frequency_dict, item):
     else:
         pass
         del frequency_dict[item]
-
-def filter_out_nonwords(token_list):
-
-    filtered_list = [token for token in token_list if isword(token)]    
-    return filtered_list
-
-def isword(token):
-
-    return pattern.match(token)
